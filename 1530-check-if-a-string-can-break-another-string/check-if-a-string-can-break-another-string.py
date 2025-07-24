f9072080377563189bd67073ace8c6f5b5481ebb
Charan Reddy <charanreddydendi212@gmail.com>
2025-07-24 09:37:00 +0530

    Time: 209 ms (27.27%) | Memory: 23.7 MB (15.15%) - LeetSync

---
+++ 1530-check-if-a-string-can-break-another-string/check-if-a-string-can-break-another-string.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def checkIfCanBreak(self, s1, s2):
+        """
+        :type s1: str
+        :type s2: str
+        :rtype: bool
+        """
+        cs1 = sorted(s1)
+        cs2 = sorted(s2)
+        return all(a >= b for a, b in zip(cs1, cs2)) or all(
+            a <= b for a, b in zip(cs1, cs2)
+        )
