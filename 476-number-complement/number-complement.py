2d54a196219419c2f64aa3440f6b9b74a1144e4d
Charan Reddy <charanreddydendi212@gmail.com>
2025-08-19 23:21:30 +0530

    Time: 4 ms (1.55%) | Memory: 18 MB (6.84%) - LeetSync

--- 476-number-complement/number-complement.py
+++ 476-number-complement/number-complement.py
@@ -1,20 +1,16 @@
 class Solution:
     def findComplement(self, num: int) -> int:
-    #binary = bin(num)
-    #    flip = binary[2:]
-     #   flipped = 
-        result = 0
-        power = 0
-    
+    #Using method
+      # binary = bin(num)[2:]
+      # flipped = ''.join('1' if b == '0' else '0' for b in binary)
+    #return int(flipped, 2) 
+        binary = ""
         while num > 0:
-    
             bit = num % 2
-            flipped_bit = 1 - bit
-        
-    
-            result += flipped_bit * (2 ** power)
-        
+            binary = str(bit) + binary
             num //= 2
-            power += 1
-    
-        return result
+        binary = binary if binary else "0"
+        flip = ''
+        for bit in binary:
+            flip += '1' if bit == '0' else '0'
+        return int(flip, 2)
