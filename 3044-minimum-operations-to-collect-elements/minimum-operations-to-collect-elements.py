934bd3d2722a0725e9ffc4f5fbbd4781425f85b9
Charan Reddy <charanreddydendi212@gmail.com>
2025-08-20 12:23:53 +0530

    Time: 3 ms (26.18%) | Memory: 18 MB (13.82%) - LeetSync

--- 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.py
+++ 3044-minimum-operations-to-collect-elements/minimum-operations-to-collect-elements.py
@@ -1,13 +1,13 @@
 class Solution:
     def minOperations(self, nums: List[int], k: int) -> int:
         collected = set()
-        op = 0
+        ops = 0
 
         for num in reversed(nums):
-            op += 1
+            ops += 1
             if 1 <= num <= k:
                 collected.add(num)
             if len(collected) == k:
                 break
 
-        return op
+        return ops
