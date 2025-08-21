3a67a19dbd8e4d0dd844dff7c5aed043462bb296
Charan Reddy <charanreddydendi212@gmail.com>
2025-08-21 19:33:54 +0530

    Time: 3 ms (6.69%) | Memory: 12.5 MB (50.52%) - LeetSync

--- 461-hamming-distance/hamming-distance.py
+++ 461-hamming-distance/hamming-distance.py
@@ -1,10 +1,11 @@
 class Solution(object):
     def hammingDistance(self, x, y):
-        k = x^y
-        c = 0
-        while k:
+
+        k = x^y 
+        count = 0 
+        while k: 
             if k&1 == 1:
-                c+=1
+                count+=1
             k>>=1
-        return c        
+        return count
         
