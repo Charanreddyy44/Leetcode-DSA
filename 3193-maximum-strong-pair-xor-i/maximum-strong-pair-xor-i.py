bb5202586faeb9b89a350c61c95beed0cda64c23
Charan Reddy <charanreddydendi212@gmail.com>
2025-08-29 13:11:17 +0530

    Time: 130 ms (23.96%) | Memory: 12.3 MB (91.67%) - LeetSync

---
+++ 3193-maximum-strong-pair-xor-i/maximum-strong-pair-xor-i.py
@@ -0,0 +1,12 @@
+class Solution(object):
+    def maximumStrongPairXor(self, nums):
+        
+        n = len(nums)
+        max_XOR = 0
+        for i in range(n):
+            for j in range(n):
+                x = nums[i]
+                y = nums[j]
+                if abs(x-y) <= min(x, y):
+                    max_XOR = max(max_XOR, x^y)
+        return max_XOR         
