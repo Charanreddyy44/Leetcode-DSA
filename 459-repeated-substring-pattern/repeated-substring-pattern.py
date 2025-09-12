22ee891bb89e2430a9f69b2a69d8d7b54745e205
Charan Reddy <charanreddydendi212@gmail.com>
2025-09-12 11:55:55 +0530

    Time: 11 ms (43.81%) | Memory: 12.5 MB (78.14%) - LeetSync

--- 459-repeated-substring-pattern/repeated-substring-pattern.py
+++ 459-repeated-substring-pattern/repeated-substring-pattern.py
@@ -4,5 +4,11 @@ class Solution(object):
         :type s: str
         :rtype: bool
         """
-        conc = (s+s)[1:-1]
-        return s in conc
+      #  conc = (s+s)[1:-1]
+       # return s in conc
+        n = len(s)
+        for i in range(1, n // 2 + 1):
+            if n % i == 0:  # i must divide n
+                if s[:i] * (n // i) == s:
+                    return True
+        return False
