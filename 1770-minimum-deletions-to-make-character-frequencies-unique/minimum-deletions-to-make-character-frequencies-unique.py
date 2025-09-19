e6463df81fb54855e00f354aa08c25f142d57a0a
Charan Reddy <charanreddydendi212@gmail.com>
2025-09-19 10:55:44 +0530

    Time: 284 ms (43.30%) | Memory: 12.8 MB (81.44%) - LeetSync

--- 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.py
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.py
@@ -5,16 +5,11 @@ class Solution(object):
         :rtype: int
         """
         S = collections.Counter(s)
-    
         count= 0
-    
         unique = set()
-    
         for char, freq in S.items():
             while freq >0 and freq in unique:
                 freq-=1
                 count+=1
-            
-            unique.add(freq)
-        
+            unique.add(freq)  
         return count
