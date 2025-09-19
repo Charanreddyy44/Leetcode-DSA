987e0a2bb467e2a0cacb9f2de2f4ab30db279ec6
Charan Reddy <charanreddydendi212@gmail.com>
2025-09-19 10:54:22 +0530

    Time: 285 ms (41.24%) | Memory: 12.9 MB (81.44%) - LeetSync

---
+++ 1770-minimum-deletions-to-make-character-frequencies-unique/minimum-deletions-to-make-character-frequencies-unique.py
@@ -0,0 +1,20 @@
+class Solution(object):
+    def minDeletions(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        S = collections.Counter(s)
+    
+        count= 0
+    
+        unique = set()
+    
+        for char, freq in S.items():
+            while freq >0 and freq in unique:
+                freq-=1
+                count+=1
+            
+            unique.add(freq)
+        
+        return count
