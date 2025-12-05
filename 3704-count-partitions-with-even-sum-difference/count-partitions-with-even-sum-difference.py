29151f7a858a692975164eeb480ff328640a0412
Charan Reddy <charanreddydendi212@gmail.com>
2025-12-05 12:49:25 +0530

    Time: 0 ms (100.00%) | Memory: 12.3 MB (82.02%) - LeetSync

--- 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
+++ 3704-count-partitions-with-even-sum-difference/count-partitions-with-even-sum-difference.py
@@ -4,12 +4,18 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        total_sum = sum(nums)
-        left_sum = 0
-        count = 0
-        for i in range(len(nums)-1):
-            left_sum += nums[i]
-            right_sum = total_sum - left_sum
-            if (left_sum % 2) == (right_sum % 2):
-                count += 1
-        return count    
+        if sum(nums) % 2 :
+            return 0
+        else:
+            return len(nums) - 1    
+
+
+       # total_sum = sum(nums)
+       # left_sum = 0
+       # count = 0
+       # for i in range(len(nums)-1):
+       #     left_sum += nums[i]
+       #     right_sum = total_sum - left_sum
+       #     if (left_sum % 2) == (right_sum % 2):
+       #         count += 1
+       # return count    
