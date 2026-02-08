8c43534349775dfa82de1ae127697edf98bf9a17
Charan Reddy <charanreddydendi212@gmail.com>
2026-02-08 13:35:35 +0530

    Time: 167 ms (8.22%) | Memory: 19.2 MB (91.54%) - LeetSync

---
+++ 4194-count-monobit-integers/count-monobit-integers.py
@@ -0,0 +1,14 @@
+class Solution:
+    def countMonobit(self, n: int) -> int:
+        count = 0
+        for i in range(n + 1):
+            b = bin(i)[2:]
+            if all(bit == b[0] for bit in b):
+                count += 1
+        return count
+        #count = 1
+        #num = 1
+        #while num <= n:
+        #   count += 1
+        #    num = (num << 1)| 1
+        #return count    
