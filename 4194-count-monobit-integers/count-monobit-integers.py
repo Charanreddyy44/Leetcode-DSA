e4d689177e2644c58e3a0147969d17e100c2096b
Charan Reddy <charanreddydendi212@gmail.com>
2026-02-08 13:37:50 +0530

    Time: 172 ms (7.52%) | Memory: 19.4 MB (28.66%) - LeetSync

--- 4194-count-monobit-integers/count-monobit-integers.py
+++ 4194-count-monobit-integers/count-monobit-integers.py
@@ -3,7 +3,7 @@ class Solution:
         count = 0
         for i in range(n + 1):
             b = bin(i)[2:]
-            if all(bit == b[0] for bit in b):
+            if all(bit == b[0]for bit in b):
                 count += 1
         return count
         #count = 1
